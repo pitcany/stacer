755e17b3e706b8f8f85a09f5c93d5ac7dc736eeb
pitcany <pitcany@gmail.com>
2025-11-12 11:52:14 -0800

    up and running with ubuntu 22.04 now

--- stacer/build/stacer/CMakeFiles/stacer.dir/stacer_autogen/EWIEGA46WW/qrc_static.cpp.o.d
+++
@@ -1,3 +0,0 @@
-stacer/CMakeFiles/stacer.dir/stacer_autogen/EWIEGA46WW/qrc_static.cpp.o: \
- /app/Stacer/build/stacer/stacer_autogen/EWIEGA46WW/qrc_static.cpp \
- /usr/include/stdc-predef.h
--- stacer/obj-x86_64-linux-gnu/stacer/CMakeFiles/stacer_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/stacer_autogen"
-  "stacer_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/stacer_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
